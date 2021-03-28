5f4ebbcdf3654b5dcc61b40d8697c6ff88f166f7
Maffinca69 <lollidark@mail.ru>
2021-03-28 14:39:33 +0400

    Deploy

---
+++ deploy.php
@@ -0,0 +1,46 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'statistic.maffinca.com');
+
+// Project repository
+set('repository', 'git@github.com:maffinca69/statisticBot.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', false);
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('193.109.78.189')
+    ->user('deployer')
+    ->identityFile('~/.ssh/deployerkey')
+    ->set('deploy_path', '/var/www/{{application}}');
+
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+task('restart:fpm', function () {
+   run('sudo /etc/init.d/php7.4-fpm restart');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+after('deploy:unlock', 'restart:fpm');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
