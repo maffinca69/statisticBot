7f5c857c6c39428f2339ef63aaf3485f9cf0e948
Maffinca69 <lollidark@mail.ru>
2021-05-05 16:38:51 +0300

    Refactoring 2

--- app/Callbacks/SelectCallback.php
+++ app/Callbacks/SelectCallback.php
@@ -4,19 +4,19 @@
 namespace App\Callbacks;
 
 
-use App\Helpers\KeyboardHelper;
 use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Exception\TelegramException;
 
 class SelectCallback extends Callback
 {
 
-    // todo: копипаст из selectCommand. Нужно добавить поддержку вызова комманд из колбэков
+    /**
+     * @return ServerResponse
+     * @throws TelegramException
+     */
     public function execute(): ServerResponse
     {
-        $keyboard = KeyboardHelper::inlineKeyboardSelectMonth();
-
-        return $this->replyToChat('Выберите месяц со статистикой', [
-            'reply_markup' => $keyboard
-        ]);
+        $command = $this->callbackQuery->getMessage()->getCommand();
+        return $this->telegram->executeCommand($command);
     }
 }
